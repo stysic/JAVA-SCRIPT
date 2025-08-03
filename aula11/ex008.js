9b29be26cee61914a8ea84efc7db664eb1819851
stysic <gdarthurbragarodrigues@gmail.com>
2025-08-03 18:24:51 -0300

    12

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { //condição simples! pois não tem ''se não (else)''
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == brasil) {
+    console.log(` Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
